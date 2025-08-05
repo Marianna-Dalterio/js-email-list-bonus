732180576e5b60e0e5343bba01f15717fe36dc69
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-08-05 12:01:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const booleanApiEndpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
+mailOutputEl = document.getElementById("emailOutput")
+
+function stampa() {
+    for (let i = 0; i < 10; i++) {
+        axios.get(booleanApiEndpoint)
+            .then(response => {
+                const email = response.data.response;
+                console.log(email)
+                mailOutputEl.innerHTML += email
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+}
+
+stampa()
+
+
